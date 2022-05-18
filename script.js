141317c16db06f53e32c69612fa0ebdf6329807d
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-05-18 08:27:12 +0700

    My life section

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  animateOut: "fadeOut",
   responsive: {
     0: {
       items: 1
