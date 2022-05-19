e4202eced1229f5eec4ae1dbf4fce33b09cd5c04
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-05-19 09:48:01 +0700

    nav-link

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
-  // autoplay: true,
+  autoplay: true,
   autoplayTimeout: 5000,
   autoplayHoverPause: true,
   animateOut: "fadeOut",
