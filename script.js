8221fa2c7784c2e72bc25b0c777a962a259810d7
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-05-25 10:36:35 +0700

    start code again

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  // autoplay: true,
-  autoplayTimeout: 5000,
-  autoplayHoverPause: true,
-  animateOut: "fadeOut",
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 1
-    },
-    1000: {
-      items: 1
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    // autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    animateOut: "fadeOut",
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
     }
-  }
-});
+  });
 
-var num = 200;
+  $(".scroll_btn_up").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-$(window).bind("scroll", function () {
-  if ($(window).scrollTop() > num) {
-    $(".header .max-width").addClass("fixed");
-  } else {
-    $(".header .max-width").removeClass("fixed");
-  }
+  var num = 200;
+
+  $(window).bind("scroll", function () {
+    if ($(window).scrollTop() > num) {
+      $(".navbar .max-width").addClass("fixed");
+    } else {
+      $(".navbar .max-width").removeClass("fixed");
+    }
+  });
 });
